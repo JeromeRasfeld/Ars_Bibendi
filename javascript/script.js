e09ca5b128f4e76aecedf9b4c09d5004ea7f9675
JeromeRasfeld <rasfeldjerome@gmail.com>
2021-03-21 17:10:08 +0100

    update 21-03

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,17 @@
 /* Mehr Anzeigen - Wein des Monats */
 function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Mehr lesen"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "wenieger Anzeigen"; 
-      moreText.style.display = "inline";
-    }
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Mehr lesen"; 
+    moreText.style.display = "none";
+  } 
+  else {
+    dots.style.display = "none";
+    btnText.innerHTML = "weniger Anzeigen"; 
+    moreText.style.display = "inline";
+  }
 }
